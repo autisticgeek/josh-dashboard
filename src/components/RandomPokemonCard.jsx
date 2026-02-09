ead999cde04835ed4cd127a82784e6523f7fe941
Steven Blakely <email@autisticgeek.com>
2026-02-09 13:59:37 -0700

    more pokemon

--- src/components/RandomPokemonCard.jsx
+++ src/components/RandomPokemonCard.jsx
@@ -75,7 +75,7 @@ export default function RandomPokemonCard() {
 
   useEffect(() => {
     async function loadRandomPokemon() {
-      const randomId = Math.floor(Math.random() * 898) + 1;
+      const randomId = Math.floor(Math.random() * 1350) + 1;
 
       const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
       const data = await res.json();
